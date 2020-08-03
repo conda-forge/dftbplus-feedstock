aad00aaaccb0be516c9f6e9265527768c19bf70e
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-08-03 11:14:59 +0200

    Add MPI support
    MNT: Re-rendered with conda-build 3.18.11, conda-smithy 3.7.5, and conda-forge-pinning 2020.08.02.15.39.57

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ "${mpi}" != "nompi" ]; then
+  MPI=ON
+  LAPACK_LIBRARIES="'lapack;blas;scalapack'"
+else
+  MPI=OFF
+  LAPACK_LIBRARIES="'lapack;blas'"
+fi
+
 cmake_options=(
    "-GNinja"
    "-DCMAKE_BUILD_TYPE=Release"
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DBUILD_SHARED_LIBS=ON"
-   "-DLAPACK_LIBRARIES='lapack;blas'"
+   "-DLAPACK_LIBRARIES=${LAPACK_LIBRARIES}"
    "-DWITH_API=ON"
    "-DWITH_SOCKETS=ON"
    "-DWITH_OMP=ON"
-   "-DWITH_MPI=OFF"
+   "-DWITH_MPI=${MPI}"
    ".."
 )
 
