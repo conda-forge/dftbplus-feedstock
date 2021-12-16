891bcc968177104c3b6d07eee18d9d3568730eda
Bálint Aradi <aradi@uni-bremen.de>
2021-12-16 09:17:33 +0100

    21.2 rebuild (#24)
    - use bundled source to obtain proper release info in header
    - add support for ChIMES
    - fix missing ARPACK support in non-mpi build
    - add build tests for included optional components
    - enable support for tblite
    
    Co-authored-by: Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,13 +27,13 @@ cmake_options=(
    "-DWITH_OMP=ON"
    "-DWITH_MPI=${MPI}"
    "-DWITH_TRANSPORT=ON"
-   "-DWITH_TBLITE=OFF"
+   "-DWITH_TBLITE=ON"
    "-DWITH_SDFTD3=ON"
    "-DWITH_MBD=ON"
    "-DWITH_PLUMED=ON"
-   "-DWITH_CHIMES=OFF"
+   "-DWITH_CHIMES=ON"
    "-GNinja"
-   ${cmake_mpi_options}
+   ${cmake_mpi_options[@]}
    ..
 )
 
@@ -43,4 +43,46 @@ pushd _build
 FFLAGS="-fno-backtrace" cmake "${cmake_options[@]}"
 ninja all install
 
+popd
+
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
+  exit 0
+fi
+
+#
+# Very quick test (< 10s) to check build sanity (checking most important components)
+#
+
+if [ "${mpi}" == "nompi" ]; then
+  ctest_mpi_regexps=(
+    'timedep/N2_onsite$'               # WITH_ARPACK
+  )
+else
+  ctest_mpi_regexps=(
+    'helical/C6H6_stack_ELPA$'         # WITH_ELSI
+  )
+  if [ "${mpi}" = "openmpi" ]; then
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+  fi
+fi
+
+ctest_regexps=(
+  'non-scc/Si_2$'
+  'transport/CH4$'                     # WITH_TRANSPORT
+  'xtb/gfn1_h2$'                       # WITH_TBLITE
+  'dispersion/2H2O_dftd3_zero$'        # WITH_SDFTD3
+  'dispersion/2C6H6_TS$'               # WITH_MBD
+  'md/H3-plumed$'                      # WITH_PLUMED
+  'chimes/CNOH$'                       # WITH_CHIMES
+  ${ctest_mpi_regexps[@]}
+)
+
+./utils/get_opt_externals slakos
+pushd _build
+for ctest_regexp in ${ctest_regexps[@]}; do
+  ctest -R "${ctest_regexp}"
+done
 popd
